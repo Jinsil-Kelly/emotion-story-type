83e57c018d7d8293bb8664dcaf2b074d669de734
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2019-12-17 12:09:17 +0000

    added body-backgroundColor transition ;)

--- src/utils/globelStyles.ts
+++ src/utils/globelStyles.ts
@@ -4,5 +4,6 @@ import { css } from '@emotion/core';
 export const makeGlobalStyles = (theme: ThemeProps) => css`
   body {
     background-color: ${theme.background};
+    transition: all 0.25s linear;
   }
 `;
